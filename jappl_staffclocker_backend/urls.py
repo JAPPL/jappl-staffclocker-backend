55f024e9a0ef72db5c3bd46861eef3354a9bc26b
sudo-lucifer <krittin.niu@student.mahidol.edu>
2023-02-27 21:51:44 +0700

    feat: add app path

--- jappl_staffclocker_backend/urls.py
+++ jappl_staffclocker_backend/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from rest_framework import routers
 
-urlpatterns = [
-    path("admin/", admin.site.urls),
-]
+context_path = "api/"
+router = routers.DefaultRouter(trailing_slash=False)
+
+urlpatterns = [path("admin/", admin.site.urls), path("{context_path}/time_log/", include('jappl_time_log.urls'))]
