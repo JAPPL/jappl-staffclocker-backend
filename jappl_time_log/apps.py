275a0634f7747caf645d7cf9e9808ff194b0ee27
sudo-lucifer <krittin.niu@student.mahidol.edu>
2023-02-27 21:41:31 +0700

    feat: init project

---
+++ jappl_time_log/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+
+
+class JapplTimeLogConfig(AppConfig):
+    """Jappl time log app config."""
+
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "jappl_time_log"
---
+++ jappl_time_log/models/__init__.py
@@ -0,0 +1,3 @@
+from jappl_time_log.models.test import Machine
+
+__all__ = ["Machine"]
