1815951353e7d0838b9e7b2ce0c87efadb5bd718
sudo-lucifer <krittin.niu@student.mahidol.edu>
2023-03-02 00:14:11 +0700

    feat: added jwt middleware

---
+++ jappl_time_log/views/protected_view.py
@@ -0,0 +1,18 @@
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+
+class ProtectedView(APIView):
+    """
+    View to list all users in the system.
+
+    * Requires token authentication.
+    * Only admin users are able to access this view.
+    """
+
+    def get(self, request: Request) -> Response:
+        """Test API for jwt token (delete this later)."""
+        print(type(format))
+        print(request.user.password)
+        return Response(data={"user_id": request.user.user_id})
