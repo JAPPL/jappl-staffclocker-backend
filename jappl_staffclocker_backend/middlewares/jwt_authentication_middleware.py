976426c79931b594bf523dca6da5dee1ca18ed84
sudo-lucifer <krittin.niu@student.mahidol.edu>
2023-03-02 00:57:51 +0700

    fix: type on exception detail

--- jappl_staffclocker_backend/middlewares/jwt_authentication_middleware.py
+++ jappl_staffclocker_backend/middlewares/jwt_authentication_middleware.py
@@ -22,6 +22,6 @@ class JWTTokenAuthentication(JWTAuthentication):
         try:
             user: UserDetail = UserDetail.objects.get(user_id=user_id)
         except self.user_model.DoesNotExist:
-            raise InvalidToken(detail="Uer not found", code="user_not_found")
+            raise InvalidToken(detail="User not found", code="user_not_found")
 
         return user
