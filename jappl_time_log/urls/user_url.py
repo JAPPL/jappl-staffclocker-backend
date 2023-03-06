ae9b1e375c8070f81e42ddb043f7790d3375a587
sudo-lucifer <krittin.niu@student.mahidol.edu>
2023-03-06 21:44:40 +0700

    fix: remove trailing slash

--- jappl_time_log/urls/user_url.py
+++ jappl_time_log/urls/user_url.py
@@ -4,6 +4,6 @@ from jappl_time_log.views.user.login_view import LoginView
 from jappl_time_log.views.user.register_view import RegisterView
 
 user_url = [
-    path("login/", LoginView.as_view(), name="login"),
-    path("register/", RegisterView.as_view(), name="register"),
+    path("login", LoginView.as_view(), name="login"),
+    path("register", RegisterView.as_view(), name="register"),
 ]
