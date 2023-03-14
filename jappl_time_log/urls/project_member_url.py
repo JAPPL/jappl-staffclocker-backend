d4aefb9f54e618e27c9a0221c619ed89225d2a73
JackKrubb <jacklee.leeco@gmail.com>
2023-03-14 18:07:54 +0700

    fix: migration

--- jappl_time_log/urls/project_member_url.py
+++ jappl_time_log/urls/project_member_url.py
@@ -4,6 +4,7 @@ from jappl_time_log.views.project_member.project_member_view import ProjectMembe
 
 project_member_url = [
     path("list", ProjectMemberViewSet.as_view({'get': 'list'}), name="list"),
+    path("detail/<int:project_member_id>", ProjectMemberViewSet.as_view({'get': 'retrieve'}), name="detail"),
     path("add", ProjectMemberViewSet.as_view({'post': 'create'}), name="add"),
     path("delete/<int:project_member_id>", ProjectMemberViewSet.as_view({'delete': 'destroy'}), name="delete"),
     path(
