5e6adde0e12755f59a6d01b9a0dd9c7783517d7a
JackKrubb <jacklee.leeco@gmail.com>
2023-03-14 17:35:50 +0700

    fix: removed admin/pm permissions and user_role in userdetail model

--- jappl_time_log/views/project/project_view.py
+++ jappl_time_log/views/project/project_view.py
@@ -1,7 +1,7 @@
 from rest_framework.viewsets import ModelViewSet
 
 from jappl_time_log.models.project_model import Project
-from jappl_time_log.permissions.is_admin_permission import IsAdminPermission
+from jappl_time_log.permissions.is_employee_permission import IsEmployeePermission
 from jappl_time_log.serializers.project.project_serializer import ProjectSerializer
 
 
@@ -10,4 +10,4 @@ class ProjectView(ModelViewSet):
 
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
-    permission_classes = [IsAdminPermission]
+    permission_classes = [IsEmployeePermission]
