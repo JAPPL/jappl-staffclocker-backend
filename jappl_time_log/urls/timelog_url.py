07335a16573c6daf845578ef7b147b3e03c68435
JackKrubb <jacklee.leeco@gmail.com>
2023-03-18 14:10:57 +0700

    feat: Add approve timelog API

--- jappl_time_log/urls/timelog_url.py
+++ jappl_time_log/urls/timelog_url.py
@@ -1,5 +1,6 @@
 from django.urls import path
 
+from jappl_time_log.views.timelog.timelog_approve_view import TimeLogApproveView
 from jappl_time_log.views.timelog.timelog_list_all_view import TimeLogListAllView
 from jappl_time_log.views.timelog.timelog_view import TimeLogView
 
@@ -9,4 +10,5 @@ timelog_url = [
     path("delete/<int:pk>", TimeLogView.as_view({'delete': 'destroy'}), name="timelog_delete"),
     path("update/<int:pk>", TimeLogView.as_view({'patch': 'partial_update', 'put': 'update'}), name="timelog_update"),
     path("all", TimeLogListAllView.as_view(), name="timelog_list_all"),
+    path("approve/<int:pk>", TimeLogApproveView.as_view(), name="timelog_approve"),
 ]
---
+++ jappl_time_log/views/timelog/timelog_approve_view.py
@@ -0,0 +1,37 @@
+from django.core.exceptions import ObjectDoesNotExist
+from django.db import transaction
+from rest_framework import status
+from rest_framework.generics import UpdateAPIView
+from rest_framework.request import Request
+from rest_framework.response import Response
+
+from jappl_time_log.models.time_log_model import TimeLog
+from jappl_time_log.models.user_detail_model import UserDetail
+from jappl_time_log.permissions.is_employee_permission import IsEmployeePermission
+from jappl_time_log.serializers.timelog.timelog_read_serializer import TimeLogSerializer
+
+
+class TimeLogApproveView(UpdateAPIView):
+    """API for approving for TimeLog."""
+
+    queryset = TimeLog.objects.all()
+    serializer_class = TimeLogSerializer
+    permission_classes = [IsEmployeePermission]
+
+    @transaction.atomic
+    def update(self, request: Request, *args, **kwargs) -> Response:
+        """Process UPDATE/PATCH (approving) timelog method for user."""
+        try:
+            query: TimeLog = TimeLog.objects.get(id=kwargs["pk"])
+            this_user: UserDetail = request.user
+            timelog_user: UserDetail = query.user_id
+
+            if timelog_user.user_id != this_user.user_id:
+                return Response(data={"Can't edit other user."}, status=status.HTTP_401_UNAUTHORIZED)
+        except ObjectDoesNotExist as e:
+            return Response(data={"detail": f"{e}"}, status=status.HTTP_400_BAD_REQUEST)
+
+        query.approved = True
+        query.save()
+        serializer = self.get_serializer(query)
+        return Response(serializer.data, status=status.HTTP_200_OK)
