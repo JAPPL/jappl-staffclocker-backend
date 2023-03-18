a240723708574bc80f9ccce9c72b549894359863
JackKrubb <jacklee.leeco@gmail.com>
2023-03-18 14:35:34 +0700

    fix: remove hashing password of user for tests

--- jappl_time_log/tests/views/timelog/test_timelog_approve_view.py
+++ jappl_time_log/tests/views/timelog/test_timelog_approve_view.py
@@ -1,5 +1,4 @@
 import json
-import secrets
 
 from django.urls import reverse
 from rest_framework import status
@@ -10,7 +9,6 @@ from rest_framework_simplejwt.tokens import RefreshToken
 from jappl_time_log.models.project_model import Project
 from jappl_time_log.models.time_log_model import TimeLog
 from jappl_time_log.models.user_detail_model import UserDetail
-from jappl_time_log.services.user.user_account_service import UserAccountService
 from jappl_time_log.tests.model_instances.project_instance import project_instance
 from jappl_time_log.tests.model_instances.time_log_instance import time_log_instance
 from jappl_time_log.tests.model_instances.user_detail_instance import user_instance
@@ -22,16 +20,14 @@ class TestTimeLogApproveView(APITestCase):
     @classmethod
     def setUpTestData(cls):
         """Mock data for timelog list test cases."""
-        raw_password: str = secrets.token_hex(16)
-        hashed_password: str = UserAccountService.hash_password(raw_password)
-        user: UserDetail = user_instance.make(email="test@gmail.com", password=hashed_password)
+        user: UserDetail = user_instance.make()
         cls.user: UserDetail = user
         cls.token: str = "Bearer " + str(RefreshToken.for_user(user=user).access_token)
         project: Project = project_instance.make(project_name="test_project")
         cls.project = project
         cls.timelog_1: TimeLog = time_log_instance.make(user_id=user, hour_spent=2, message="test", project_id=project)
 
-        user_2: UserDetail = user_instance.make(email="test2@gmail.com", password=hashed_password)
+        user_2: UserDetail = user_instance.make()
         cls.user_2: UserDetail = user_2
         cls.token_2: str = "Bearer " + str(RefreshToken.for_user(user=user_2).access_token)
         cls.timelog_2: TimeLog = time_log_instance.make(
