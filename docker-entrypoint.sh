46a1e3613204b8c5ba8f7c517d23309851a65a34
sudo-lucifer <krittin.niu@student.mahidol.edu>
2023-03-24 23:39:33 +0700

    fix: dockerfile

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
+set -e
+
 python manage.py migrate
-gunicorn jappl_staffclocker_backend.wsgi:application --bind 0.0.0.0:8000 --timeout 600 --preload
+#gunicorn jappl_staffclocker_backend.wsgi:application --bind 0.0.0.0:8000 --timeout 600 --preload
+exec "$@"
